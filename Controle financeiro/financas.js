b02b5d2e720a5f9cf71ae4da35689c92ca10ee55
daniwegmann <124391230+daniwegmann@users.noreply.github.com>
2023-07-19 10:30:47 -0300

    Add files via upload

---
+++ Controle financeiro/financas.js
@@ -0,0 +1,27 @@
+let saldo = prompt("Informe a quantidade de dinheiro inicial disponível:")
+saldo = parseFloat(sald0)
+let opcao = ""
+
+do {
+    opcao = prompt(
+        "Saldo disponivel: R$ " + saldo +
+        "\n1. Adicionar dinheiro" +
+        "\n2. Remover dinheiro" +
+        "\n3. Sair"
+    )
+
+    switch (opcao) {
+    case "1":
+        saldo += parseFloat(prompt("Informe o valor a ser adicionado:"))
+        break
+    case "2":
+        saldo -= parseFloat("Informe o valor a ser removido:")
+        break
+    case "3":
+        alert("Saindo...")
+        break
+    defaul:
+        alert("Entrada inválida!")
+    }
+
+} while (opcao !== "3")
